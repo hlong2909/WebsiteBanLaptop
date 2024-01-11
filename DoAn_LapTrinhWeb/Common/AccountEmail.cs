1f5eb269348a0ba219d5673e4d06798a6b753e83
hlong2909 <156309266+hlong2909@users.noreply.github.com>
2024-01-11 19:10:16 +0700

    WebsitBanMayTinh

---
+++ DoAn_LapTrinhWeb/Common/AccountEmail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn_LapTrinhWeb.Common
+{
+    public class AccountEmail
+    {
+        //vui lòng không đổi mật khẩu mail, néu mật khảu bị thay đổi vui lòng nhắn tin qua zalo:0828568959
+        //để dùng google email gửi email reset cho người khác bạn cần phải vô đây "https://www.google.com/settings/security/lesssecureapps" Cho phép ứng dụng kém an toàn: Bật
+        public const string Host = "mail8971.maychuemail.com"; //tên mấy chủ nếu bạn dùng gmail thì đổi  "Host = "smtp.gmail.com"
+        //tài khoản email của bạn
+        public const string UserEmail = "no-reply@nvtcomputer.tech";
+        public const string UserEmailSupport = "support@nvtcomputer.tech";
+        //Mật khẩu Email của bạn
+        public const string Password = "123456a@";
+        //Tên Email bạn muốn hiển thị khi gửi
+        public const string Name = "NVTComputer";
+    }
+}
